2eebe27921edc29932ab89764a1c7a18e2ecc0be
Hibanaw Hu <hibanaw@qq.com>
2023-10-29 11:08:11 +0800

    Add control.

---
+++ control.c
@@ -0,0 +1,26 @@
+#include "control.h"
+
+
+void control(){
+    if(doMotor){
+        motorOutput(2000, 2000);
+    }
+    if(doSteer){
+        pidControl();
+    }
+}
+
+void pidControl()
+{
+    static int32 preError;
+    static int32 outputI;
+    int32 outputP;
+    int32 outputD;
+    output = 0;
+    outputP = error * servoKp/1000;
+    outputI += error * servoKi/1000;
+    outputD = (error - preError) * servoKd/1000;
+    output = outputP + outputI + outputD;
+    preError = error;
+    steerOutput(output);
+}
---
+++ control.h
@@ -0,0 +1,11 @@
+#ifndef _CONTROL_H_
+#define _CONTROL_H_
+
+#include "zf_common_headfile.h"
+#include "param.h"
+#include "servo.h"
+
+void control();
+void pidControl();
+
+#endif
