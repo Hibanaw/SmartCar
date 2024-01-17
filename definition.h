d33db46823df8f1f5a82c169e1379e72e868947a
Hibanaw Hu <hibanaw@qq.com>
2024-01-17 19:38:02 +0800

    使用TC264的引脚GPIO定义

--- definition.h
+++ definition.h
@@ -1,19 +1,19 @@
 #ifndef _DEFINITION_H_
 #define _DEFINITION_H_
 
-#define PIN_BUTTON_0    D0
-#define PIN_BUTTON_1    D1
-#define PIN_BUTTON_2    D2
-#define PIN_BUTTON_3    D3
+#define PIN_BUTTON_0    P11_3
+#define PIN_BUTTON_1    P20_7
+#define PIN_BUTTON_2    P11_2
+#define PIN_BUTTON_3    P20_6
 
 #define PIN_SWITCH_1    (D14)
 #define PIN_SWITCH_2    (D15)
 
-#define PIN_MOTORL_A    (TIM5_PWM_CH1_A0)
+#define PIN_MOTORL_A    (ATOM1_CH2_P21_4)
 #define PIN_MOTORL_B    (TIM5_PWM_CH3_A2)
-#define PIN_MOTORR_A    (TIM5_PWM_CH2_A1)
+#define PIN_MOTORR_A    (ATOM1_CH3_P21_5)
 #define PIN_MOTORR_B    (TIM5_PWM_CH4_A3)
 
-#define PIN_STEER       (TIM2_PWM_CH1_A15)
+#define PIN_STEER       (ATOM0_CH1_P33_9)
 
 #endif
