a5c0c1fafa20b6747bc05513a97a6f2731b19ed3
Hibanaw Hu <hibanaw@qq.com>
2024-01-18 11:52:16 +0800

    增加根据状态动态调控速度，命名规范

--- control.c
+++ control.c
@@ -5,21 +5,47 @@
 #include "encoder.h"
 #include "param.h"
 #include "servo.h"
+#include "image.h"
 
-void control(){
-    if(doMotor){
+int32 lMotorOutput;
+int32 rMotorOutput;
+
+void control()
+{
+    if (doMotor)
+    {
+        calcMotorTargrt();
         motorControl();
     }
-    if(doSteer){
+    if (doSteer)
+    {
         steerControl();
     }
 }
 
-void motorControl(){
+void calcMotorTargrt()
+{
+    switch (routeState)
+    {
+    case UPHILL:
+        speedTarget = speed * 1.5;
+        break;
+    case STOP:
+        speedTarget = 0;
+        break;
+    case STRAIGHT:
+    case CROSS:
+        speedTarget = speed;
+    default:
+        speedTarget = 0.8 * speed;
+        break;
+    }
+}
+
+void motorControl()
+{
     static float lError[3];
     static float rError[3];
-    static int32 lOutput;
-    static int32 rOutput;
     int32 lPreOuput;
     int32 lOutputP;
     int32 lOutputI;
@@ -30,27 +56,26 @@ void motorControl(){
     int32 rOutputI;
     int32 rOutputD;
 
-    lPreOuput = lOutput;
+    lPreOuput = lMotorOutput;
     lError[2] = lError[1];
     lError[1] = lError[0];
     lError[0] = speedTarget - lSpeed;
     lOutputP = motorKp * (lError[0] - lError[1]);
     lOutputI = motorKi * lError[0];
-    lOutputD = motorKd * (lError[0]-2*lError[1]+lError[2]);
-    lOutput = lOutputP + lOutputI + lOutputD + lPreOuput;
+    lOutputD = motorKd * (lError[0] - 2 * lError[1] + lError[2]);
+    lMotorOutput = lOutputP + lOutputI + lOutputD + lPreOuput;
 
-    rPreOuput = rOutput;
+    rPreOuput = rMotorOutput;
     rError[2] = rError[1];
     rError[1] = rError[0];
     rError[0] = speedTarget - rSpeed;
     rOutputP = motorKp * (rError[0] - rError[1]);
     rOutputI = motorKi * rError[0];
-    rOutputD = motorKd * (rError[0]-2*rError[1]+rError[2]);
-    rOutput = lOutputP + lOutputI + lOutputD + lPreOuput;
+    rOutputD = motorKd * (rError[0] - 2 * rError[1] + rError[2]);
+    rMotorOutput = lOutputP + lOutputI + lOutputD + lPreOuput;
 
-    motorOutput(lOutput, rOutput);
+    motorOutput(lMotorOutput, rMotorOutput);
 }
-
 
 void steerControl()
 {
--- control.h
+++ control.h
@@ -1,7 +1,13 @@
 #ifndef _CONTROL_H_
 #define _CONTROL_H_
 
+#include "zf_common/zf_common_typedef.h"
+
+extern int32 lMotorOutput;
+extern int32 rMotorOutput;
+
 void control();
+void calcMotorTargrt();
 void motorControl();
 void steerControl();
 
