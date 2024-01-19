35e1c8ff9ccd3edf608d04ce38b6ccd7543b56c2
Hibanaw Hu <hibanaw@qq.com>
2024-01-19 14:38:20 +0800

    取消Switch使用

--- switch.c
+++ switch.c
@@ -8,11 +8,11 @@ uint8 switch1 = 0;
 uint8 switch2 = 0;
 
 void switchInit(){
-    gpio_init(PIN_SWITCH_1, GPI, GPIO_HIGH, GPI_FLOATING_IN);
-    gpio_init(PIN_SWITCH_2, GPI, GPIO_HIGH, GPI_FLOATING_IN);
+//    gpio_init(PIN_SWITCH_1, GPI, GPIO_HIGH, GPI_FLOATING_IN);
+//    gpio_init(PIN_SWITCH_2, GPI, GPIO_HIGH, GPI_FLOATING_IN);
 }
 
 void switchScan(){
-    switch1 = gpio_get_level(PIN_SWITCH_1);
-    switch2 = gpio_get_level(PIN_SWITCH_2);
+//    switch1 = gpio_get_level(PIN_SWITCH_1);
+//    switch2 = gpio_get_level(PIN_SWITCH_2);
 }
--- switch.h
+++ switch.h
@@ -1,6 +1,8 @@
 #ifndef _SWITCH_H_
 #define _SWITCH_H_
 
+#include "zf_common_typedef.h"
+
 extern uint8 switch1;
 extern uint8 switch2;
 
