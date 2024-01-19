a09f17b4262e73377d5af5fe94a7d543fefdcd1c
Hibanaw Hu <hibanaw@qq.com>
2024-01-19 14:36:43 +0800

    TC264 flash

--- flash.c
+++ flash.c
@@ -4,15 +4,15 @@
 #include "param.h"
 void readParameterFromFlash(void)
 {
-  flash_read_page_to_buffer(126, 3);
-  if(flash_union_buffer[255].int32_type != -1){
+  flash_read_page_to_buffer(126, 11);
+  if(flash_union_buffer[1023].int32_type != -1){
     memset(flash_union_buffer, 0, sizeof(flash_union_buffer));
-    flash_union_buffer[255].int32_type = -1;
+    flash_union_buffer[1023].int32_type = -1;
   }
 }
 
 void saveParameterToFlash(void)
 {
-  flash_erase_page(126, 3);
-  flash_write_page_from_buffer(126, 3);
+  flash_erase_page(126, 11);
+  flash_write_page_from_buffer(126, 11);
 }
